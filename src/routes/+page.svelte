b8286610a58d73cdb4c1a0bcb3ca53dd21eda17a
BlueOrcaz <bondoldman@gmail.com>
2025-09-27 18:35:30 +1000

    basic css elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
+    import background from "../static/Background.jpg";
+
     import FileUpload from "$lib/FileUpload.svelte";
     import { decodeFile } from "$lib/decodeSaveFile";
     let decodedResult = null;
 
     // default
     let timePlayed = "0 h 00 min 00 sec"; // default
-    let health = 5;
-    let silk = 0;
+    let maskShards = 5;
+    let maxSilk = 0;
     let silkHearts = 0;
     let rosaries = 0;
     let shellShards = 0;
@@ -21,8 +23,8 @@
             );
             timePlayed = formatTime(playTimeInSeconds);
 
-            health = decodedResult.playerData.maxHealth;
-            silk = decodedResult.playerData.silkMax;
+            maskShards = decodedResult.playerData.maxHealth;
+            maxSilk = decodedResult.playerData.silkMax;
             silkHearts = decodedResult.playerData.silkRegenMax;
             rosaries = decodedResult.playerData.geo;
             shellShards = decodedResult.playerData.ShellShards;
@@ -41,37 +43,42 @@
     }
 </script>
 
-<div>
-    <div class="flex place-content-center">
-        <h1 class="font-bold underline">
-            Hollow Knight: Silksong Save Completion Analyser
-        </h1>
+<div
+    class="bg-fixed h-screen w-full bg-cover bg-center relative"
+    style="background-image: url({background});"
+>
+    <!-- Overlay to darken the background -->
+    <div class="absolute inset-0 bg-black opacity-25"></div>
+
+    <div class="relative z-10 flex place-content-center">
+        <h1 class="text-5xl text-white mt-7 pt-6">Save Completion Analyser</h1>
     </div>
 
-    <FileUpload on:fileLoaded={handleFile} />
-
-    <h2>Game Status:</h2>
-    <h2>True Completion:</h2>
-
-    <p>Time Played: {timePlayed}</p>
-
-    <div>
-        <p>Health: {health}</p>
-        <p>Silk: {silk}</p>
-        <p>Rosaries: {rosaries}</p>
-        <p>Shell Shards: {shellShards}</p>
+    <div class="relative flex place-content-center">
+        <h1 class="text-white pb-5">For Hollow Knight: Silksong</h1>
     </div>
 
-    <div class="relative group w-fit">
-        <!-- Text -->
-        <p class="text-lg font-bold">Secret Message 👀</p>
-
-        <!-- Cover -->
-        <div class="absolute inset-0 bg-gray-800 group-hover:opacity-0 transition-opacity duration-300"></div>
+    <div class="relative flex place-content-center text-white pb-10">
+        <FileUpload on:fileLoaded={handleFile} />
     </div>
 
-    <div>
-  <h1 class="font-bold">Bosses</h1>
-  
-</div>
+    <div class="flex items-center justify-center">
+        <div class="text-center">
+            <h2 class="text-white">Game Status:</h2>
+            <h2 class="text-white">True Completion:</h2>
+
+            <p class="text-white">Time Played: {timePlayed}</p>
+
+            <div>
+                <p class="text-white">Mask Shards: {maskShards}</p>
+                <p class="text-white">Max Silk: {maxSilk}</p>
+                <p class="text-white">Rosaries: {rosaries}</p>
+                <p class="text-white">Shell Shards: {shellShards}</p>
+            </div>
+
+            <div>
+                <h1 class="font-bold text-white">Bosses</h1>
+            </div>
+        </div>
+    </div>
 </div>
