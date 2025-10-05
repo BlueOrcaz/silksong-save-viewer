8b591972ba8dd69db2ce6dc026e05988fa3b134f
BlueOrcaz <bondoldman@gmail.com>
2025-10-05 15:37:22 +1000

    added in vercel analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
+  import { injectAnalytics } from '@vercel/analytics/sveltekit'
   import background from "../static/Background.jpg";
-
   import FileUpload from "$lib/components/FileUpload.svelte";
   import { decodeFile } from "$lib/decodeSaveFile";
 
